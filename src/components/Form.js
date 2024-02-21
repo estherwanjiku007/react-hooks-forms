b0e0d29709ea23a28305ee62e031ad9006b43b85
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-02-21 09:31:31 +0300

    aster

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,16 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
-
+   function handleFirstName(event){
+    setFirstName(event.target.value)
+   }
+   function handleLastName(event){
+    setLastName(event.target.value)
+   }
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstName} value={firstName} />
+      <input type="text" onChange={handleLastName}value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
